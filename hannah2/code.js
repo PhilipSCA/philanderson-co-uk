8870c3301f98a4e9a27da3993f7fae7626c5b715
PhilipSCA <philanderson556@gmail.com>
2020-12-31 18:11:00 +0000

    deleted hannah3 and hannah2 and added shake animation to happy new year text on main index.html

--- hannah2/code.js
+++
@@ -1,8 +0,0 @@
-
-
-$query = "INSERT INTO comments(post_id,username,comment) VALUES ('$post_id','$commentorname','$comment')";
-mysqli_query($con,$query);
-
-$query = "SELECT * FROM comments WHERE post_id='{$id}'";
-$result = mysqli_query($con,$query);
-$row = mysqli_fetch_array($result);
--- hannah2/file.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $con= mysqli_connect("localhost","root","","database_name");
-?>
-
-$query = "INSERT INTO comments(post_id,username,comment) VALUES ('$post_id','$commentorname','$comment')";
-mysqli_query($con,$query);
