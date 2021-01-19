1cb3c093bb2f9846a760542835eef8f5a01051ca
BuildTools <unconfigured@null.spigotmc.org>
2021-01-19 21:17:31 +0000

    changed the submit and then changed it back

--- hannah/itscomplete.php
+++ hannah/itscomplete.php
@@ -15,6 +15,14 @@ if(!$quantity){
 <html>
     <h1 style="color:white">After the that you payed...</h1>
     <p style="color: white">You can now Send me your files of your pictures.</p>
+
+    
+    <link rel="icon" href="images/icon5.png">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <title>Dream to Decor | DTD - Decorations, paint, walls, tiles</title>
+    <!--AdUqMkrW2rogd7xux-5XqctWLRHf7Up_qThlNfVban-mbK6EZQG6lIZbJVZq6wycIflPVaM4qfP7rndU-->
+    <!-- <script src="https://www.paypal.com/sdk/js?client-id=AaExbpJ1m0YUGwbLMnUQLKCQSehxH3CEd58emyz7fC1F6bVwGt6-Q8ZEBu0hi0v9Q3zkJa2dgsSHDjhI"></script> -->
+  
     <style>
         .file{
             color: magenta;
