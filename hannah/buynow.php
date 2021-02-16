3128059abb38afbc72bfd7f5e1889cd1b23b52b2
PhilipSCA <philanderson556@gmail.com>
2021-02-16 08:06:06 +0000

    addded a pay shoppy button

--- hannah/buynow.php
+++ hannah/buynow.php
@@ -1,4 +1,16 @@
 <html>
 
+<form action="purchase.php" method="POST">
+<label for="email">Email Address</label>
+<input id="email" name="email" type="text" placeholder="Enter your email here"></input>
+<br>
+<label for="cardcode">Card Code</label>
+<input id="cardcode" name="cardcode" type="text" placeholder="Enter your gift card code here"></input>
+<br>
+<input type="submit"></input>
+</form>
+
+<script src="https://shoppy.gg/api/embed.js"></script>
+<button data-shoppy-product="Jlxcu8K">Pay</button>
 
 </html>
