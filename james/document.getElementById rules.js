2ebda0fea5cf5673abe63fcef223ea19bd592151
PhilipSCA <philanderson556@gmail.com>
2022-02-14 15:38:29 +0000

    added doc rules

---
+++ james/document.getElementById rules.js
@@ -0,0 +1,9 @@
+document.getElementById().value = "";
+/* Is for form fields */
+
+document.getElementById().innerHTML = ""; 
+// Is for paragraphs, divs and other HTML elements
+
+document.getElementById().className = "";
+/* Is for assigning a class to an element which replaces any classes that
+the element has been assigned in the static markup */
