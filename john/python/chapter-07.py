b373a51f0a9ae9e7513dc243e9fb3290ec64b83d
Philip <philanderson556@gmail.com>
2022-04-07 16:58:58 +0100

    python

---
+++ john/python/chapter-07.py
@@ -0,0 +1,12 @@
+date = "Thursday 07/04/22"
+print(date)
+total_cost = 1 + 3 * 4
+# The answer is 1 + 12 = 13
+total_cost = (1 + 3) * 4
+# The answer is (4) x 4 = 16
+result_of_computation = (2 * 4) * 4 + 2
+
+result_of_computation = ((2 * 4) * 4) + 2
+# the answer here is (8 x 4) + 2 = 32
+result_of_computation = (2 * 4) * (4 + 2)
+# the answer here is (8) x (6) = 48
---
+++ john/python/chapter-09.py
@@ -0,0 +1,11 @@
+date = "Thursday 07/04/22"
+print(date)
+species = "cat"
+if species == "cat":
+    status = "ok"
+    kingdom = "animal"
+    print("Yep, it's cat.")
+
+if 2 + 2 == 4:
+    print("Everything makes sense.")
+    
