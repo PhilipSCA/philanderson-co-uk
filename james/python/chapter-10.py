aaf21725b21cdb32f49a3f84fcf59fd4b80f1a00
Gaming PC <philanderson556@gmail.com>
2022-04-07 16:49:27 +0100

    chapter 10

--- james/python/chapter-10.py
+++ james/python/chapter-10.py
@@ -1,2 +1,19 @@
 
 # Chapter 10: Comparison Operators
+
+# == is a comparison operator, can be used to compare various combination (Case sensitive)
+# != means not equal to, its the opposite of ==, can compare various combinations (Case sensitive)
+# e.g This statement is true: Rose != rose and rose == rose
+# These are 4 more comparison operators, usually used to compare numbers
+# > - Greater than
+# < - Less than
+# >= Greater than or equal to
+# <= Less than or equal to
+# All the statements below are true
+
+# if 1 > 0:
+# if 0 < 1:
+# if 1 >= 0:
+# if 1 >= 1:
+# if 0 <= 1:
+# if 1 <= 1:
