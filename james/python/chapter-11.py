68681336211415d0a32ea6925809d5cfc2bd1683
Gaming PC <philanderson556@gmail.com>
2022-04-09 09:54:16 +0100

    Done chapter 11

--- james/python/chapter-11.py
+++ james/python/chapter-11.py
@@ -2,15 +2,35 @@
     # Chapter 11: Else and elif statements
 
 # In the if statements I did before, if it was false nothing happened.
-drink = "Milk"
+drink = "milk"
 if drink == "water":
     print("Yep, its water")
 if drink != "water":
     print("Nope, its not water")
-
-# But a simpler way to do this is using 'else:' because if it isnt assigned water, of course its not water
+    
+# But a simpler way to do this is using 'else:' because if it isnt assigned water, of course its not water.
 
 if drink == "water":
     print("Yep, its water")
 else:
     print("Nope, its not water")
+
+# Finally, there's 'elif'. Its short for 'else if'. If no test has been successful, elif tries something else.
+
+if drink == "water":
+    print("Yep, its water")
+elif drink == "milk":
+    print("Oh, its actually milk")
+else:
+    print("Nope, its not water or milk")
+
+# You can have any number of elif statements.
+# You would never have any more than one else statement as its at the end.
+
+# If you dont want to stop testing even after one is successful, you just keep using if.
+if drink == "water":
+    print("Yep, its water")
+if drink == "milk":
+    print("Oh, its actually milk")
+if drink == "juice":
+    print("Nope, its not water or milk")
