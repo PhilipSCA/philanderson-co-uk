9dfb985223f8f91e41be5d9eb0d189f0b2e1f370
Gaming PC <philanderson556@gmail.com>
2022-04-12 14:44:42 +0100

    done chapter 14 and 15

---
+++ james/python/chapter-14.py
@@ -0,0 +1,17 @@
+# Tuesday 12/4/22
+#    Chapter 14: Comments
+
+# You comment using the "#" symbol in python
+# Comments are for the reader/programmer, not the computer
+# I have been using comments from chapter 1, but this is where i learn them
+# Python ignores comments
+
+# This is a comment
+# This is another comment
+
+# To write a multi line comment
+
+'''
+This is a comment
+This is another comment
+'''
---
+++ james/python/chapter-15.py
@@ -0,0 +1,35 @@
+# Tuesday 12/4/22
+#    Chapter 15: Lists
+
+city_0 = "Atlanta"
+city_1 = "Baltimore"
+city_2 = "Chicago"
+city_3 = "Denver"
+city_4 = "Los Angeles"
+
+# If i do
+print("Welcome to " + city_2)
+# Python will display: Welcome to Chicago
+
+# Another type of variable is lists, it can have a sequence of values assigned to it.
+# The values are known as 'elements'
+
+cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles"]
+# cities is the variable
+# When you are making a list, you enclose everything on the right in square brackets.
+
+# Once you have made a list with a variable, you can refer to each element like this:
+print(cities[0])
+print(cities[1])
+# cities[0] would be Atlanta
+# cities[1] would be Baltimore
+# The numbers start from 0, not 1, so 0 is the first element.
+# You enclose the number in square brackets
+
+# A list value can be any value you assign to ordinary variables
+
+random_things = [2, "Bob", "Now is"]
+
+# random_things[0] = 2 (Numerical value)
+# random_things[1] = "Bob"
+# random_things[2] = "Now is"
