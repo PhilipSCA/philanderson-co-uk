63979ea74c69a6b7e6bae43bf0a2e86d6eea308b
Gaming PC <philanderson556@gmail.com>
2022-05-23 19:37:07 +0100

    done 25

---
+++ james/python/chapter-25.py
@@ -0,0 +1,16 @@
+# Monday 23/5/22
+#   Dictionaries: What they are
+
+# Say you wanted to make a list but it was a bit more complicated
+print ("Customer 155's first name: James")
+print ("Customer 155's last name: Anderson")
+print ("Customer 155's address: (random address)")
+print ("Customer 155's city: London")
+print ("Customer 155's country: England")
+
+# When you are working with Customer 155's information, you want to be able to pick something out of the series.
+# So you create a dictionary
+
+# A dictionary works something like a list, but instead of a simple series of things, its a series of pairs(2).
+# Each pair contains a key - (e.g "first name"), and a value - ("James")
+
