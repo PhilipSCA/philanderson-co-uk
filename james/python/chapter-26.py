f2e13dc933261e30d65a6d4c65e566432afb3a1b
Gaming PC <philanderson556@gmail.com>
2022-05-24 19:43:09 +0100

    chapter-26.py

---
+++ james/python/chapter-26.py
@@ -0,0 +1,14 @@
+# Tuesday 24/5/22
+#   Coding Dictionaries
+
+# If we use the same example as the previous chapter (Customer 155), this is how we would make the dictionary.
+# In a dictionary, its similar to a list but we use curly brackets, and each thing is paired key and value (colon)
+customer_115 = {"first name": "James", "last name": "Anderson", "address": "2203 random address"}
+
+# Also, the variable name is usually singular, not plural
+# And everything, key and value, is in quotation marks
+
+# This is basically a longer version of a dictionary:
+first_name = "James"
+last_name = "Anderson"
+address = "2203 random address"
