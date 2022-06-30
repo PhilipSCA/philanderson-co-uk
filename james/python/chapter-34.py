93b86aab8121c28040fb56257d746c5335c22594
philanderson888 <philanderson888@hotmail.com>
2022-06-30 21:22:29 +0100

    chapter 34

---
+++ james/python/chapter-34.py
@@ -0,0 +1,40 @@
+# Thursday 30/6/22
+#   Creating a list of dictionaries
+
+customer_155 = {
+  "first name": "James", 
+  "last name": "Anderson", 
+  "address": "2203 random address"
+ }
+
+ # All that we have done has been for one dictionary, but in times you would need multiple dictionaries
+ # Lets say each dictionary represents a person in a family of 5.
+ # You create a list of five dictionaries, one for each.
+
+family_members = [
+  {
+    "family id": 0,
+    "first name":"Hannah",
+    "last name":"Anderson.",
+  },
+  {
+    "family id": 1,
+    "first name":"John",
+    "last name":"Anderson.",
+  }
+  {
+    "family id": 2,
+    "first name":"James"
+    "last name":"Anderson."
+  },
+  {
+    "family id": 3,
+    "first name":"Philip"
+    "last name":"Anderson."
+  },
+  {
+    "family id": 4,
+    "first name":"Christa"
+    "last name":"Anderson."
+  },
+] 
