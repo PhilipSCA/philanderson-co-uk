a22348dbc2094f350d9b042d086f31913f54d91c
PhilipSCA <philanderson556@gmail.com>
2022-07-18 16:51:48 +0100

    Dictionaries

---
+++ john/python/chapter-30.py
@@ -0,0 +1,11 @@
+date = "Monday 18/07/22"
+print(date) 
+
+# you can delete from a list aswell
+customer_29 = {"first name": "David", "last name": "Elliot", "address": "4308 Wellesley St."}
+del customer_29["address"]
+print(customer_29)
+# you can also change stuff in a dictionary
+customer_291 = {"first name": "David", "last name": "Elliot", "address": "4308 Wellesley St.", "city": "Toronto"}
+customer_291["city"] = "Winipeg"
+# The value of "city", Toronto has now changed to Winipeg
---
+++ john/python/chapter-31.py
@@ -0,0 +1,7 @@
+date = "Monday 18/07/22"
+print(date)
+
+customer_29 = {"first name": "David", "last name": "Elliot", "address": "4308 Wellesley St.", "city": "Toronto"}
+# to loop through a dictionary to display all the info:
+for each_value in customer_29.values():
+    print(each_value)
