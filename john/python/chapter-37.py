3d8f80148ce268de8c387be4f79cb9f031a293b9
PhilipSCA <philanderson556@gmail.com>
2022-07-25 14:04:43 +0100

    Lists in dictionaries

---
+++ john/python/chapter-37.py
@@ -0,0 +1,10 @@
+date = "Monday 25/07/22"
+print(date)
+
+# to put a list in dictionary:
+customer_29 = {
+    "first name": "David",
+    "last name": "Elliot",
+    "address": "4803 Wellesley St.",
+    "discounts": ["standard", "volume", "loyalty"], 
+}
