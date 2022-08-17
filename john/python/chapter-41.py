c3ca8fec4f3a38913fd786a036c168e1f63537af
PhilipSCA <philanderson556@gmail.com>
2022-08-17 16:52:53 +0100

    Functions

---
+++ john/python/chapter-41.py
@@ -0,0 +1,14 @@
+date = "Wednesday 17th August 2022"
+print(date)
+
+# Functions!
+
+def add_numbers():
+    first_number = 2
+    second_number = 3
+    total = first_number + second_number
+    print(total)
+
+# This is how you write a function. Now when you want this code to run you just need to type add_numbers
+
+print(add_numbers())
