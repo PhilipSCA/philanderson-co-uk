96b04cb177b4f4df3ea4468c392083e7b969c54d
philanderson888 <philanderson888@hotmail.com>
2022-08-27 19:31:49 +0100

    else and elif

--- john/python/chapter-11.py
+++ john/python/chapter-11.py
@@ -5,14 +5,12 @@ species = "dog"
 if species == "cat":
     print("Yep, its cat.")
 if species != "cat":
-    print("Nope, not cat.")
+    print("After using an if statement, Nope, not cat.")
 # the easier way is using else:
 if species == "cat":
     print("Yep, its cat.")
 else:
-    print("Nope, not cat.")
-
-
+    print("After using an else statement, Nope, not cat.")
 
 donut_condition = "not fresh" 
 donut_price = "low"
@@ -23,3 +21,8 @@ if donut_condition == "fresh":
 elif donut_price == "low":
     buy_score += 5
 print("buy score is " + str(buy_score))
+
+# Friday 08/04/22
+# After using an if statement, Nope, not cat.
+# After using an else statement, Nope, not cat.
+# buy score is 5
