33e1e8b3e2f41b606ae75b738536e72231a80453
philanderson888 <philanderson888@hotmail.com>
2022-08-27 20:37:13 +0100

    Dictionaries

--- john/python/chapter-25-26.py
+++ john/python/chapter-25-26.py
@@ -3,10 +3,15 @@ print(date)
 
 # this is a list
 my_cats = ["Draco", "Bellatrix", "Voldemort"]
-print(my_cats[0])
+print("this is from a list: " + str(my_cats[0]))
 
 # this is a list compared to a dictionary:
 # list
 jobs_to_do_1st = ["email", "texting", "calls"]
 # dictionary
 customer_29 = {"first name": "David", "last name": "Elliot", "address": "4308 Wellesley St."}
+print("This is from a dictionary: " + customer_29["last name"])
+
+# Saturday 16/07/22
+# this is from a list: Draco
+# This is from a dictionary: Elliot
