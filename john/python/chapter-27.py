61b6c33dcf8264b8ba196f5aa8d2ba44bb49145a
philanderson888 <philanderson888@hotmail.com>
2022-08-27 20:38:47 +0100

    Dictionaries

--- john/python/chapter-27.py
+++ john/python/chapter-27.py
@@ -4,4 +4,7 @@ print(date)
 customer_29 = {"first name": "David", "last name": "Elliot", "address": "4308 Wellesley St."}
 
 address_of_customer = customer_29["address"]
-print(address_of_customer)
+print("customer 29's address is " + address_of_customer)
+
+# Saturday 16/07/22
+# customer 29's address is 4308 Wellesley St.
