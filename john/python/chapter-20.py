d8c1c84d1c8eaece5deff55b5da51139d7e1dd8b
philanderson888 <philanderson888@hotmail.com>
2022-08-27 20:08:58 +0100

    tuples

--- john/python/chapter-20.py
+++ john/python/chapter-20.py
@@ -4,3 +4,6 @@ print(date)
 states_in_order_of_founding = ("Delaware", "Pennyslyvania", "New Jersey", "Georgia")
 second_state_founded = states_in_order_of_founding[1]
 print("The second state founded was " + second_state_founded)
+
+# Tuesday 28/06/22
+# The second state founded was Pennyslyvania
