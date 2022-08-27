47e1d16b1117bde1a1e9a284d41508377fc3f82c
philanderson888 <philanderson888@hotmail.com>
2022-08-27 19:20:32 +0100

    Operations

--- john/python/chapter-04.py
+++ john/python/chapter-04.py
@@ -10,3 +10,9 @@ print("loss is " + str(loss))
 print("dozens is " + str(dozens))
 print("popular number is " + str(popular_number))
 print("total is " + str(total))
+
+# Wednesday 06/04/22
+# loss is -12
+# dozens is 36
+# popular number is 3.0
+# total is 200.075 
