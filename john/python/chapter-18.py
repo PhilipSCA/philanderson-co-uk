3763103796c2a325534c60ab0cc3952c2495c410
philanderson888 <philanderson888@hotmail.com>
2022-08-27 20:02:39 +0100

    removing elements

--- john/python/chapter-18.py
+++ john/python/chapter-18.py
@@ -3,7 +3,7 @@ print(date)
 
 tasks = ["email Frank", "call Sarah", "meet with Zach"]
 del tasks[0]
-print(tasks)
+print("tasks when deleting using del is " + str(tasks))
 
 tasks2 = ["email Frank", "call Sarah", "meet with Zach"]
 del tasks[1]
@@ -11,5 +11,9 @@ del tasks[1]
 
 # you can also use .remove
 
-tasks.remove("email Frank")
-print(tasks)
+tasks2.remove("email Frank")
+print("tasks when deleting using .remove is " + str(tasks2))
+
+# Saturday 25/06/22
+# tasks when deleting using del is ['call Sarah', 'meet with Zach']
+# tasks when deleting using .remove is ['call Sarah', 'meet with Zach']
