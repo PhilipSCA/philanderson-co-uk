70004763e0387f28977368bb3a9daadfccf57c28
philanderson888 <philanderson888@hotmail.com>
2022-08-27 19:22:47 +0100

    brackets(maths)

--- john/python/chapter-07.py
+++ john/python/chapter-07.py
@@ -15,3 +15,8 @@ result_of_computation2 = (2 * 4) * (4 + 2)
 print("total cost is " + str(total_cost))
 print("8x4+2 is " + str(result_of_computation))
 print("(8) x (6) is " + str(result_of_computation2))
+
+# Thursday 07/04/22
+# total cost is 16
+# 8x4+2 is 34
+# (8) x (6) is 48
