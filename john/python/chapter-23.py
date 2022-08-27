2a6c15998a09220ecb89bb62f467bb0af8363773
philanderson888 <philanderson888@hotmail.com>
2022-08-27 20:20:55 +0100

    converting

--- john/python/chapter-23.py
+++ john/python/chapter-23.py
@@ -7,3 +7,7 @@ monthly_income_as_an_integer = int(monthly_income)
 min_wage = 15
 min_wage = str(min_wage)
 print("The minimum wage in your state is $" + min_wage)
+
+# Thursday 14/07/22
+# Enter your monthly income: 5
+# The minimum wage in your state is $15
