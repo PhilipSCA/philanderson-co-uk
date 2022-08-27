587acc0b4e99c7a4d8910d50d0f941f097ad8b14
philanderson888 <philanderson888@hotmail.com>
2022-08-27 19:25:20 +0100

    If statement

--- john/python/chapter-09.py
+++ john/python/chapter-09.py
@@ -5,8 +5,11 @@ species = "cat"
 if species == "cat":
     status = "ok"
     kingdom = "animal"
-    print("Yep, it's cat.")
+    print("after using an if statement, Yep, it's cat.")
 
 if 2 + 2 == 4:
-    print("Everything makes sense.")
+    print("after using an if statement, Everything makes sense.")
     
+# Thursday 07/04/22
+# after using an if statement, Yep, it's cat.
+# after using an if statement, Everything makes sense.
