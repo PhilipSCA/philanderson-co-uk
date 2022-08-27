337b0ba67b8b7c9f50444949d4b183ba7653ab76
Gaming PC <philanderson556@gmail.com>
2022-08-27 10:38:52 +0100

    james - add output to chapter 6

--- james/python/chapter-06.py
+++ james/python/chapter-06.py
@@ -2,7 +2,9 @@ print ('python - Chapter 6 - Unfamiliar math operators')
 print ('==============================================')
 print('8 April 2022')
 
-print (' modulo operator (percent symbol) gives you remainder after division')
+print(" ")
+print(" ")
+print ('modulo operator (percent symbol) gives you remainder after division')
 
 x = 15
 y = 4
@@ -21,17 +23,35 @@ print("When there is no remainder, its assigned 0")
 
 
 # Addition (this is 4):
-age = 3
-age = age + 1
+age = 8
+age = age + 9
 # or 
-age2 = 3 
-age2 += 1
+age2 = 12
+age2 += 19
 
 # It is the same as Addition for subtraction and multiplication just use -/* instead of +
 # You can add variables
 
-age3 = 5
-amount_to_increment = 4
-total = age3 + amount_to_increment # = 9
+age3 = 66
+amount_to_increment = 44
+total = age3 + amount_to_increment 
+
+print(" ")
+print(" ")
+print("These are two variables being added together")
 print(str(age3) + ' + ' + str(amount_to_increment) + " = " + str(total))
-print("This is 2 variables being added together to make 9")
+
+
+# python - Chapter 6 - Unfamiliar math operators
+# ==============================================
+# 8 April 2022
+
+
+# modulo operator (percent symbol) gives you remainder after division
+# The remainder of 15 % 4 = 3
+# 16 % 4 = 0
+# When there is no remainder, its assigned 0
+
+
+# These are two variables being added together
+# 66 + 44 = 110
