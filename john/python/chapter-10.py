909a7a2549f39e51d8d504407ef6f28eb0e18d71
philanderson888 <philanderson888@hotmail.com>
2022-08-27 19:28:37 +0100

    Comparisons

--- john/python/chapter-10.py
+++ john/python/chapter-10.py
@@ -3,7 +3,7 @@ print(date)
 
 your_ticket_number = 487209
 if your_ticket_number != 487208:
-    print("Better luck next time.")
+    print("Your ticket number was not " + str(your_ticket_number) + ", Better luck next time.")
 
 # == is not an equal sign, its a comparison operator
 # != means 'not equal to'
@@ -11,3 +11,6 @@ if your_ticket_number != 487208:
 # < means 'less than'
 # >= means greater than or equal to
 # <= means less than or equal to
+
+# Thursday 07/04/22
+# Your ticket number was not 487209, Better luck next time.
