dbf6658b498bc53d2cb5583f3caba2317e58c51d
philanderson888 <philanderson888@hotmail.com>
2022-08-27 20:41:21 +0100

    keys & values

--- john/python/chapter-28.py
+++ john/python/chapter-28.py
@@ -4,11 +4,11 @@ print(date)
 # keys dont have to be strings, they can be numbers
 rankings = {5: "Finland", 2: "Norway", 3: "Sweden", 7:"Iceland"}
 second_ranked_country = rankings[2]
-print(second_ranked_country)
+print("the second ranked country is " + second_ranked_country)
 
 country_rankings = {"Finland": 5, "Norway": 2, "Sweden": 3, "Iceland": 7}
 norway_rank = country_rankings["Norway"]
-print(norway_rank)
+print("Norways rank is " + str(norway_rank))
 
 # it is better to write it like this:
 rankings = {
@@ -17,3 +17,7 @@ rankings = {
     3: "Sweden", 
     7: "Iceland",
 }
+
+# Monday 18/07/22
+# the second ranked country is Norway
+# Norways rank is 2
