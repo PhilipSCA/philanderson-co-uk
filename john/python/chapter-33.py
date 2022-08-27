de49321a1b541303f913a72f989c401d51c16fe5
philanderson888 <philanderson888@hotmail.com>
2022-08-27 21:11:37 +0100

    for loops

--- john/python/chapter-33.py
+++ john/python/chapter-33.py
@@ -11,3 +11,8 @@ for each_key, each_value in customer_29.items():
     print("The customer's " + each_key + " is " + each_value)
 
 # each_key and each_value are variables
+
+# Tuesday 19/07/22
+# The customer's first name is David
+# The customer's last name is Elliot
+# The customer's address is 4803 Wellesley St.
