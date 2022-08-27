cb0f08a465ce09f63b7bc4ed563f9c3eead7d12c
philanderson888 <philanderson888@hotmail.com>
2022-08-27 21:05:10 +0100

    for loops

--- john/python/chapter-32.py
+++ john/python/chapter-32.py
@@ -7,6 +7,12 @@ customer_29 = {
     "address": "4803 Wellesley St.",
 }
 # to loop the keys in a dictionary:
+print("Each key in the dictionary using a for loop is:")
 for each_key in customer_29.keys():
     print(each_key)
 
+# Tuesday 19/07/22
+# Each key in the dictionary using a for loop is:
+# first name
+# last name
+# address
