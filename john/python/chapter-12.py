8a67789427ff4d25bbc74b8d163eda98adcf0b8c
philanderson888 <philanderson888@hotmail.com>
2022-08-27 19:37:51 +0100

    conditions

--- john/python/chapter-12.py
+++ john/python/chapter-12.py
@@ -4,7 +4,7 @@ print(date)
 weight = 300
 time = 6
 if weight <= 300 and time <= 6:
-    status = "try to recruit him"
+    status = "Under or equal to 300 pounds and 6 seconds to run, try to recruit him"
     print(status)
 
 salary = 52000
@@ -14,8 +14,11 @@ has_a_lamborghini = "yes"
 if salary > 60000 or bonus > 10000 or has_a_lamborghini == "yes":
     wealth = "rich"
 
-if (salary < 60000 and bonus < 10000) or has_a_lamborghini == "no":
+elif (salary < 60000 and bonus < 10000) or has_a_lamborghini == "no":
     wealth = "not rich"
 
-
 print("he is " + wealth)
+
+# Friday 08/04/22
+# Under or equal to 300 pounds and 6 seconds to run, try to recruit him
+# he is rich
