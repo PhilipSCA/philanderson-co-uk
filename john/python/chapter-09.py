033542164f10cc6695d39aa70d59d48c86d8bf81
PhilipSCA <philanderson556@gmail.com>
2022-08-28 15:46:58 +0100

    separators

--- john/python/chapter-09.py
+++ john/python/chapter-09.py
@@ -1,5 +1,6 @@
 date = "Thursday 07/04/22"
 print(date)
+print("If statements")
 
 species = "cat"
 if species == "cat":
@@ -11,5 +12,6 @@ if 2 + 2 == 4:
     print("after using an if statement, Everything makes sense.")
     
 # Thursday 07/04/22
+# If statements
 # after using an if statement, Yep, it's cat.
 # after using an if statement, Everything makes sense.
