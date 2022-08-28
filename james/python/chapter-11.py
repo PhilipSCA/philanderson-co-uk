a41e371c87b8f3e03a21072938500e5ddecb61c8
Gaming PC <philanderson556@gmail.com>
2022-08-28 17:49:03 +0100

    chapters 11, 12 and 13

--- james/python/chapter-11.py
+++ james/python/chapter-11.py
@@ -1,13 +1,11 @@
-# Saturday 9/4/22
-    # Chapter 11: Else and elif statements
 print('python - Chapter 11 - Else and elif statements')
 print('==============================================')
 print('9 April 2022')
 
 print()
 
-# In the if statements I did before, if it was false nothing happened.
 drink = "milk"
+print(f'if..else..statement setting drink = {drink}')
 if drink == "water":
     print("Yep, its water")
 if drink != "water":
@@ -15,20 +13,19 @@ if drink != "water":
 
 print()
 
-print("If a condition is met, something happens, but if the condition isnt met, something also has to happen.")
-print("Testing if drink is water")
+print('If a condition is met, something happens, but if the condition isnt met, something also has to happen')
+print('Testing if drink is water using `else` statement')
 print("Drink is " + drink + "If drink = water, print ' Yep, its water." "If drink isnt (!=) water, print 'Nope, it's not water'")
-# But a simpler way to do this is using 'else:' because if it isnt assigned water, of course its not water.
 
 print()
-
+print (f'drink is {drink} ... let us run this through an else statement')
 if drink == "water":
     print("Yep, its water")
 else:
     print("Nope, its not water")
 
 print("In if statements the last if is else:")
-# Finally, there's 'elif'. Its short for 'else if'. If no test has been successful, elif tries something else.
+print ('Finally, there is elif. Its short for "else if". If no test has been successful, elif tries something else.')
 
 if drink == "water":
     print("Yep, its water")
@@ -50,3 +47,24 @@ if drink == "juice":
 
 print()
 print("Drink is being tested if it is water, if its not, it tests whether its milk, if its not again, it tests if its juice.")
+
+
+# python - Chapter 11 - Else and elif statements
+# ==============================================
+# 9 April 2022
+
+# if..else..statement setting drink = milk
+# Nope, its not water
+
+# If a condition is met, something happens, but if the condition isnt met, something also has to happen
+# Testing if drink is water using `else` statement
+# Drink is milkIf drink = water, print ' Yep, its water.If drink isnt (!=) water, print 'Nope, it's not water'
+
+# drink is milk ... let us run this through an else statement
+# Nope, its not water
+# In if statements the last if is else:
+# Finally, there is elif. Its short for "else if". If no test has been successful, elif tries something else.
+# Oh, its actually milk
+# Oh, its actually milk
+
+# Drink is being tested if it is water, if its not, it tests whether its milk, if its not again, it tests if its juice.  
--- james/python/chapter-13.py
+++ james/python/chapter-13.py
@@ -1,5 +1,7 @@
-# Saturday 9/4/22
-    # if statements nested
+print('python - Chapter 13 - if statements nested')
+print('==============================================')
+print('April 2022')
+print()
 
 x = "a"
 y = "b"
@@ -10,13 +12,13 @@ e = "f"
 d = "g"
 h = "h"
 f = "e"
-
+g = 'I'
 if (x == y or a == b) and c == d:
     g = h
 else:
     e = f
-
-# Another way to write this is using nesting
+print(f"If ({x} = {y} or {a} = {b}) and {c} = {d}, then {g} is equal to {h}")
+print("Another way to write this is using nesting")
 if c == d:
     if x == y:
         g = h
@@ -26,6 +28,16 @@ if c == d:
         e = f
 else:
     e = f
-
+print()
+print('When you use nesting, instead of using "and", you just create a new if or elif statement.')
 # If the first line if is true, the next 6 lines execute.
 # If the first line is false, the next 6 lines are skipped.
+
+# python - Chapter 13 - if statements nested
+# ==============================================
+# April 2022
+
+# If (a = b or c = d) and e = g, then I is equal to h
+# Another way to write this is using nesting
+
+# When you use nesting, instead of using "and", you just create a new if or elif statement.
