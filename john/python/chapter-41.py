89fb209324201b4059b1568c447a8762fedbbffa
philanderson888 <philanderson888@hotmail.com>
2022-08-29 17:31:05 +0100

    functions

--- john/python/chapter-41.py
+++ john/python/chapter-41.py
@@ -10,5 +10,12 @@ def add_numbers():
     print(total)
 
 # This is how you write a function. Now when you want this code to run you just need to type add_numbers
+add_numbers()
 
-print(add_numbers())
+print("From a function: ") 
+add_numbers()
+
+# Wednesday 17th August 2022
+# 5
+# From a function:
+# 5
