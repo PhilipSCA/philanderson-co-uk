4d772c61da3adc4b9f9f385e93b605b0ae406013
philanderson888 <philanderson888@hotmail.com>
2022-08-30 15:01:09 +0100

    functions

--- john/python/chapter-50.py
+++ john/python/chapter-50.py
@@ -10,3 +10,10 @@ def say_something():
   what_to_say = "Hi"
   now_say_it(what_to_say)
 
+print("The function inside a function is told to print: ")
+say_something()
+
+# Thursday 25/08/22
+# The function inside a function is told to print:
+# Hi
+
