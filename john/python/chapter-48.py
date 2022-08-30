ab2c3a6208e80569f6bf5b2d30a8db3c0e8ffc1b
philanderson888 <philanderson888@hotmail.com>
2022-08-30 14:57:17 +0100

    condensing code

--- john/python/chapter-48.py
+++ john/python/chapter-48.py
@@ -4,14 +4,14 @@ print(date)
 # Condensing code
 def calc_tax(sales_total, tax_rate):
   tax = sales_total * tax_rate
-  print(tax)
+  return(tax)
 
 # Instead of writing:
 sales_tax = calc_tax(sales_total=101.37, tax_rate=.05)
-print(sales_tax)
+print("calc tax printed using a variable: " + str(sales_tax))
 
 # Write this:
-print(calc_tax(sales_total=101.37, tax_rate=.05))
+print("calc tax condensed and printed without a variable: " + str(calc_tax(sales_total=101.37, tax_rate=.05)))
 
 # Another example
 def add_numbers(first_number, second_number):
@@ -27,3 +27,10 @@ sum_of_results = result_of_adding + result_of_subtracting
 
 # You could make it into 1 simple line - Write this:
 sum_of_results = add_numbers(1, 2) + subtract_numbers(3, 2)
+
+print("sum of results is " + str(sum_of_results))
+
+# Thursday 25/08/22
+# calc tax printed using a variable: 5.0685
+# calc tax condensed and printed without a variable: 5.0685
+# sum of results is 4
