8ad6c2c5391b4f05dc3459fa4ab55aee4b52e504
philanderson888 <philanderson888@hotmail.com>
2022-08-30 14:59:36 +0100

    local & global

--- john/python/chapter-49.py
+++ john/python/chapter-49.py
@@ -1,3 +1,6 @@
+from re import S
+
+
 date = "Thursday 25/08/22"
 print(date)
 
@@ -5,8 +8,15 @@ print(date)
 
 # This is a global variable, you can use it anywhere
 what_to_say = "Hi"
-
+print("This is a global variable: " + what_to_say)
 # A local variable is one in a function, so what_to_say is local here:
 def say_something():
   what_to_say = "Hi"
-# You cant use the local variable outside of the function.kk 
+  print("This is a local variable in a function: " + what_to_say)
+
+say_something()
+# You cant use the local variable outside of the function.
+
+# Thursday 25/08/22
+# This is a global variable: Hi
+# This is a local variable in a function: Hi 
