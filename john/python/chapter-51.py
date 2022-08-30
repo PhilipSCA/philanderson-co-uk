431e25b08d0f327a6d870cb4dbe1bbe8a7dbe0aa
philanderson888 <philanderson888@hotmail.com>
2022-08-30 16:48:09 +0100

    while loops

--- john/python/chapter-51.py
+++ john/python/chapter-51.py
@@ -3,20 +3,54 @@ print(date)
 
 # While Loops
 
-# This is a for loop, it checks one thing and sees if its on the list.
+# This is a while loop, it checks one thing and sees if its on the list.
 cleanest_cities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"]
-city_to_check = input("Enter the name of a city: ")
-for a_clean_city in cleanest_cities:
-      if city_to_check == a_clean_city:
-        print("It's one of the cleanest cities")
-      break
 
-# This is a while loop. The difference is this allows the user to check another city after another or stop by pressing "q". If
+
+
+# get city from user
 user_input = ""
 while user_input != "q":
   user_input = input("Enter a city, or q to quit:")
+  print(user_input)
+  print(cleanest_cities)
+
+
+
+
+
+
+  # check user input against list of valid cities
+  # loop over all cities and if match is found set flag to TRUE
   if user_input != "q":
+    match_found = False
+    
     for a_clean_city in cleanest_cities:
       if user_input == a_clean_city:
-        print("It's one of the cleanest cities")
-      break
+        match_found = True
+        break
+    
+
+
+
+
+    # finally, see if we found a MATCH
+    print(match_found)
+    if match_found == True:
+      print("Its one of the cleanest cities.")
+    else:
+      print("This is not one of the cleanest cities.")
+
+
+# Thursday 25/08/22
+# Enter a city, or q to quit:London
+# London
+# ['Cheyenne', 'Santa Fe', 'Tucson', 'Great Falls', 'Honolulu']
+# False
+# This is not one of the cleanest cities.
+# Enter a city, or q to quit:Tucson
+# Tucson
+# ['Cheyenne', 'Santa Fe', 'Tucson', 'Great Falls', 'Honolulu']
+# True
+# Its one of the cleanest cities.
+# Enter a city, or q to quit:q 
