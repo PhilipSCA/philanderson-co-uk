c186ce166f3dcd2196aab8432d25ff8720863488
philanderson888 <philanderson888@hotmail.com>
2022-09-02 14:13:43 +0100

    all

--- john/python/all.py
+++ john/python/all.py
@@ -162,6 +162,34 @@ def chapter_58():
     print(f"Fourth patient - {pid1298.first_name} {pid1298.last_name}, age {pid1298.age}")
     print(" ")
 
+#============================================================================================
+
+def chapter_59():
+    print("")
+    print("Friday 02/09/22")
+    print("Using methods for functions")
+    print("====================================")
+    print("")
+
+    # Methods
+    class Patient:
+      def __init__(self, last_name, first_name, age):
+        self.last_name = last_name
+        self.first_name = first_name
+        self.age = age
+
+    # function to check if patients are over 21:
+      def age_check(self):
+        if self.age > 21:
+            print(self.first_name + " " + self.last_name + " is not a minor, she is over 21.")
+        else:
+         print(self.first_name + " " + self.last_name + " is a minor, they are younger than 21.")
+
+    pid4343 = Patient("Taleb", "Sue", 61)
+
+    # This is a method:
+    pid4343.age_check()
+
 #=============================================================================================================================================================================================================================================
 
 
@@ -189,3 +217,6 @@ while user_input != "q":
 
     if user_input == "58":
         chapter_58()
+
+    if user_input == "59":
+        chapter_59()
