ab680a6b02c44abf10269f841fdef2798a1418e4
philanderson888 <philanderson888@hotmail.com>
2022-09-05 14:13:59 +0100

    getting data

--- john/python/chapter-64.py
+++ john/python/chapter-64.py
@@ -7,7 +7,7 @@ print("")
 # This is to read a file, the difference is there is a 'r' instead of a 'w'
 # 'w' is for write and 'r' is for read
 
-with open("chapter-62-63.py-greet.txt", "r") as file_to_use:
+with open("chapter-62-greet.txt", "r") as file_to_use:
     text_of_file = file_to_use.read()
 
 print("The text in the file is " + text_of_file)
