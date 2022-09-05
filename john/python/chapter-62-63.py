a0257bd077acebe2c673fe57ea8488bf8768d29f
philanderson888 <philanderson888@hotmail.com>
2022-09-05 14:13:29 +0100

    data files

--- john/python/chapter-62-63.py
+++ john/python/chapter-62-63.py
@@ -6,12 +6,12 @@ print("")
 
 # To save a word processing document or spreadsheet(data) on python:
 
-with open("greet.txt", "w") as file_to_use:
+with open("chapter-62-greet.txt", "w") as file_to_use:
     file_to_use.write("Hello World")
 
 # This is creating a file and putting the text "Hello World in it. The file automatically closes because of "with".
 # If there is an existing file called "greet.txt" already then it will delete everything because of "w" and put the new text 
 # You can also use variables:
 greeting = "Hello World"
-with open("greet.txt", "w") as file:
+with open("chapter-62-greet.txt", "w") as file:
     file.write(greeting)
