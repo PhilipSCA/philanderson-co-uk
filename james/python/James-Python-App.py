db734519dafbd2df6e5c6af85e1c22a2df38826e
Gaming PC <philanderson556@gmail.com>
2022-12-23 17:48:54 +0000

    James-Python-App (Simple)

---
+++ james/python/James-Python-App.py
@@ -0,0 +1,13 @@
+print("James's Python app")
+print()
+
+num = int(input("Enter a number - 1,2 or 3: "))
+
+if num == 1:
+    print("Hello World!")
+elif num == 2:
+    print("My name is James")
+else:
+    print("Goodbye World!")
+
+
