7287cb4693b8d2a3d5dd4811523ce9ee815beaf8
PhilipSCA <philanderson556@gmail.com>
2023-08-10 14:15:52 +0100

    Hannah commit 10/08

---
+++ hannahpython/whileloop.py
@@ -0,0 +1,26 @@
+#10/08/23 22 lines
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+a = 1
+while a < 6:
+  print(a)
+  if (a == 3):
+    break
+  a += 1
+
+b = 0
+while b < 6:
+  b += 1
+  if b == 3:
+    continue
+  print(b)
+
+j = 1
+while j < 6:
+  print(j)
+  j += 1
+else:
+  print("j is no longer less than 6")
