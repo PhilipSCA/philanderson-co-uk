977a1596914b161abe249ddb749c7eb42a1b81f9
PhilipSCA <philanderson556@gmail.com>
2024-06-17 14:01:20 +0100

    project 2 squares

---
+++ john/python-codewars/project-02-squares.py
@@ -0,0 +1,17 @@
+def square_digits(num):
+    # input number
+    # convert to string
+    num = num.string()
+    num = [num.length(1), num.length(2)]
+    x = 0
+    num[x]
+    x = x + 1
+    
+    # split string into individual characters
+    # convert each character back into integer
+    # square each integer
+    # convert squared number back into a string
+    # concatenate all the strings
+    # output final concatenated string
+    
+    
